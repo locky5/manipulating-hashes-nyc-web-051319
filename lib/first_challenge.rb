3735fed414976ce9dfba95c1b31f4fa52dfbd195
Kevin Dai <daikevin19@yahoo.com>
2019-05-04 06:39:02 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,13 +13,13 @@ def first_challenge
     }
   }
 
-    contacts.each do |person, data|
-      data.each do |attribute, value|
-        if attribute == :favorite_icecream_flavors
-          value.delete_if {|flavor| flavor == "strawberry"}
-        end
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if {|flavor| flavor == "strawberry"}
       end
     end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
